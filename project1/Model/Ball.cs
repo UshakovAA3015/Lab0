070eae130a4401f2a926a4f2513206652c250a4e
Alexander <ushakov3015@gmail.com>
2025-10-21 16:38:41 +0700

    feature(Model): Реализация классов геометрических фигур

---
+++ project1/Model/Ball.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    /// <summary>
+    /// Шар
+    /// </summary>
+    public class Ball : FigureBase
+    {
+
+        /// <summary>
+        /// Радиус шара
+        /// </summary>
+        private double _radiusBall;
+
+        /// <summary>
+        /// Свойство - радиус шара
+        /// </summary>
+        public double Radius
+        {
+            get
+            {
+                return _radiusBall;
+            }
+            set
+            {
+                CheckNumber(value);
+                _radiusBall = value;
+            }
+        }
+
+        /// <summary>
+        /// Свойство - вычисление объёма шара
+        /// </summary>
+        /// <retutns>Объём шара</retutns>
+
+        public override double Volume
+        {
+            get
+            {
+                return (4.0 / 3.0) * Math.PI * Math.Pow(Radius, 3);
+            }
+        }
+    }
+}
---
+++ project1/Model/Parallelepiped.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    /// <summary>
+    /// Параллелепипед
+    /// </summary>
+    public class Parallelepiped : FigureBase
+    {
+        /// <summary>
+        /// Длина
+        /// </summary>
+        private double _length;
+
+        /// <summary>
+        /// Ширина
+        /// </summary>
+        private double _width;
+
+        /// <summary>
+        /// Высота
+        /// </summary>
+        private double _height;
+
+        /// <summary>
+        /// Свойство - длина
+        /// </summary>
+        public double Length
+        {
+            get
+            {
+                return _length;
+            }
+            set
+            {
+                CheckNumber(value);
+                _length = value;
+            }
+        }
+
+        /// <summary>
+        /// Свойство - ширина
+        /// </summary>
+        public double Width
+        {
+            get
+            {
+                return _width;
+            }
+            set
+            {
+                CheckNumber(value);
+                _width = value;
+            }
+        }
+
+        /// <summary>
+        /// Свойство - высота
+        /// </summary>
+        public double Height
+        {
+            get
+            {
+                return _height;
+            }
+            set
+            {
+                CheckNumber(value);
+                _height = value;
+            }
+        }
+
+        /// <summary>
+        /// Свойство - вычисление объёма параллелепипеда
+        /// </summary>
+        /// <retutns>Объём паралеллепипеда</retutns>
+        public override double Volume
+        {
+            get
+            {
+                return Length * Width * Height;
+            }
+        }
+    }
+}
