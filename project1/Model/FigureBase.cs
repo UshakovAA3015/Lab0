0f860ba6f34c2d6280ba6ca8ca77c792ba9a6cc8
Alexander <ushakov3015@gmail.com>
2025-10-21 15:51:54 +0700

    feature(Model): Добавлен базовый класс FigueBase

---
+++ project1/Model/FigureBase.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    /// <summary>
+    /// Базовый класс 
+    /// для всех объёмных фигур
+    /// </summary>
+    public abstract class FigureBase
+    {
+        /// <summary>
+        /// Свойство для расчёта объёма
+        /// </summary>
+        public abstract double Volume { get; }
+
+        /// <summary>
+        /// Метод проверки числа
+        /// </summary>
+        /// <param name="number">Число для проверки</param>
+        /// <returns>Корректное число</returns>
+        public static double CheckNumber(double number)
+        {
+            if (number < 0)
+            {
+                throw new ArgumentOutOfRangeException("Величина должна " +
+                    "быть положительным числом!");
+            }
+            else
+            {
+                return number;
+            }
+        }
+    }
+}
